1283039bb959adedfb702663ba8eaf6eb72bf4be
Daniil Pershin <pershin.daniil.e@gmail.com>
2022-08-05 01:43:15 +0300

    fix: update parser.js

--- src/parser.js
+++ src/parser.js
@@ -4,6 +4,7 @@ import { FILE_TYPE } from './constants.js';
 const parsers = {
   [FILE_TYPE.JSON]: (content) => (content ? JSON.parse(content) : ''),
   [FILE_TYPE.YAML]: (content) => (content ? yaml.load(content) : ''),
+  [FILE_TYPE.YML]: yaml.load,
 };
 
 export default (content, typeName) => parsers[typeName](content);
